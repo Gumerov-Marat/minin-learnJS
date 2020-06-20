4c08bc45794875dae2db8e4fa58310c722a1b34d
Marat Gumerov <gumerovm74@gmail.com>
2020-06-20 15:00:47 +0300

    lesson-1

---
+++ src/script.js
@@ -0,0 +1,22 @@
+// //задание 1 Доступ к элементам
+// var div = document.getElementById('playground');
+
+// var p = document.getElementsByClassName('text');
+
+// var h1 = document.getElementsByTagName('h1');
+
+
+// console.log(h1);
+// console.log(div);
+// console.log(p);
+
+//более новые методы
+
+// id из css = #id
+var div = document.querySelector('#playground');
+var p = document.querySelectorAll('.text')
+var h1 = document.querySelectorAll('h1')
+
+console.log(h1);
+console.log(div);
+console.log(p);
