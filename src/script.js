434abae770e5bfa3b1a398ab9b78d50632cd0262
Marat Gumerov <gumerovm74@gmail.com>
2020-06-21 01:40:05 +0300

    dom-5 Всплытие и погружение

--- src/script.js
+++ src/script.js
@@ -1,25 +1,7 @@
-var button = document.querySelector('button')
-var h1 = document.querySelector('h1')
-var input = document.querySelector('input')
+var divs = document.querySelectorAll('div')
 
-
-// создаем функцию для события (что должно произойти)
-function buttonhandler() {
-  //console.log('clicked!')
-  h1.textContent = input.value;
+for (var i = 0; i < divs.length; i++){
+  divs[i].addEventListener('click', function(){
+    console.log(this.getAttribute('id'));
+  },true)
 }
-
-// добавляем событие на кнопку
-//button.addEventListener('click', buttonhandler)
-
-// реализация с анонимной функцией
-h1.addEventListener('mouseenter',function () {
-  console.log('Мышка зашла в заголовок h1');
-  this.style.color = 'red'
-})
-
-h1.addEventListener('mouseleave', function () {
-  console.log('Мышка ушла с  h1');
-  this.style.color = 'black'
-
-})
