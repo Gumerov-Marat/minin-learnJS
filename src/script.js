abee8698abb928c1bf0584d1b83d387cd9d9a0e6
Marat Gumerov <gumerovm74@gmail.com>
2020-06-21 14:44:33 +0300

    Методы для массивов

--- src/script.js
+++ src/script.js
@@ -1,11 +1,20 @@
-document.querySelector('#alert').addEventListener('click', function(){
-  alert('Вы успешно кликнули по кнопке');
-  
+var str = '1,2,3,4,5,6,7,8'
+
+// сплит с сепаратором ,
+var array = str.split(',')
+console.log(array);
+
+// обратное действие
+//array.join(';');
+
+var objArr = [
+  {name:'Max', age: 27},
+  {name: 'Elena', age: 18},
+  {name: 'Victor', age: 20}
+]
+
+var foundPerson = objArr.find(function(person){
+  return person.age ===20
 })
 
-document.querySelector('#confirm').addEventListener('click', function () {
-  var decision = confirm('Вы успешно кликнули по кнопке');
-  console.log(decision);
-  
-
-})
+console.log(foundPerson);
