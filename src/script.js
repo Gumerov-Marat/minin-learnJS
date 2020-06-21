2ac92ac9e31a4e34ce2ff71af87227f02eeefc17
Marat Gumerov <gumerovm74@gmail.com>
2020-06-21 13:44:52 +0300

    async defer

--- src/script.js
+++ src/script.js
@@ -1,29 +1,8 @@
-//делигирование
-
 /*
-  мы можем ставить прослушку события на каждый элемент,но можем...
+атрибут  async  дает команду загружать скрипт немедленно
+не ожидая ничего
+(если у нас скрипт перед боди то ускоряет работу)
+
+defer - атрибут такой же как асинк, но если есть 
+несколько defer порядок их  загрузки и запуска сохранится.
 */
-
-var p = document.querySelectorAll('p')
-
-for (var i = 0; i < p.length; i++){
-  p[i].addEventListener('click', function(event){
-    event.target.style.color = 'blue'
-  })
-};
-
-/*
-  ищем внутри wrapper  по тегу p
-*/
-
-document.getElementById('wrapper').addEventListener('click', function(event){
-  var tagname = event.target.tagName.toLowerCase();
-
-  if (tagname === 'p') {
-    event.target.style.color = 'blue'
-  }
-  // те у кого класс колор
-  if (event.target.classList.contains('color')){
-    event.target.style.color = 'red'
-  }
-})
