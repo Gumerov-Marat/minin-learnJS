136e5957fa404e990733c4965ec4f1fc2e4b4b11
Marat Gumerov <gumerovm74@gmail.com>
2020-06-22 12:12:26 +0300

    step-2: add figure

--- src/index.js
+++ src/index.js
@@ -1,15 +1,38 @@
 var $start = document.querySelector('#start')
 var $game = document.querySelector('#game')
 
-$start.addEventListener('click', startGame)
+var score = 0
 
+$start.addEventListener('click', startGame)
+$game.addEventListener('click', handleBoxClick)
+
+// функция старта игры
 function startGame() {
   $game.style.backgroundColor = '#fff'
   $start.classList.add('hide')
-
+  score++
   renderBox()
 }
 
-function renderBox() {
+function handleBoxClick(event){
+  if(event.target.dataset){
+    renderBox()
+  }
+  
+}
 
+// Создание квадрата
+function renderBox() {
+  $game.innerHTML = ''
+  var box = document.createElement('div')
+
+  box.style.height = box.style.width = '50px'
+  box.style.position = 'absolute'
+  box.style.backgroundColor = '#000'
+  box.style.top = '50px'
+  box.style.left ='70px'
+  box.style.cursor = 'pointer'
+  box.setAttribute('data-box', 'true')
+
+  $game.insertAdjacentElement('afterbegin', box)
 }
