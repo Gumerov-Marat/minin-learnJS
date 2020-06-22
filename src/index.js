d905b018654acb87aae87c84d38f64658e96997f
Marat Gumerov <gumerovm74@gmail.com>
2020-06-22 14:18:47 +0300

    step-5: Вывод результата

--- src/index.js
+++ src/index.js
@@ -1,47 +1,82 @@
 var $start = document.querySelector('#start')
 var $game = document.querySelector('#game')
 var $time = document.querySelector('#time')
+var $result = document.querySelector('#result')
+var $timeHeader = document.querySelector('#time-header')
+var $resultHeader = document.querySelector('#result-header')
+var $gameTime = document.querySelector('#game-time')
 
 var score = 0
 var isGameStarted = false
 
 $start.addEventListener('click', startGame)
 $game.addEventListener('click', handleBoxClick)
+$gameTime.addEventListener('input', setGameTime)
 
-// функция старта игры
+function show($el) {
+  $el.classList.remove('hide')
+}
+
+function hide($el) {
+  $el.classList.add('hide')
+}
+
+
 function startGame() {
+  score = 0
+  setGameTime()
+  $gameTime.setAttribute('disabled', 'true')
   isGameStarted = true
   $game.style.backgroundColor = '#fff'
-  $start.classList.add('hide')
+  hide($start)
 
-  var interval = setInterval(function(){
-    var time = parseFloat ($time.textContent)
+  var interval = setInterval(function () {
+    var time = parseFloat($time.textContent)
 
-    if (time <=0){
+    if (time <= 0) {
       clearInterval(interval)
       endGame()
     } else {
-      $time.textContent = (time - 0.1).toFixed(1);
+      $time.textContent = (time - 0.1).toFixed(1)
     }
-  }, 100);
+  }, 100)
 
   renderBox()
 }
 
-function handleBoxClick(event){
-  if (!isGameStarted) {
-    return
-  }
-  if(event.target.dataset){
-    renderBox()
-  }
+function setGameScore() {
+  $result.textContent = score.toString()
+}
+
+function setGameTime() {
+  var time = +$gameTime.value
+  $time.textContent = time.toFixed(1)
+  show($timeHeader)
+  hide($resultHeader)
 }
 
 function endGame() {
   isGameStarted = false
+  setGameScore()
+  $gameTime.removeAttribute('disabled')
+  show($start)
+  $game.innerHTML = ''
+  $game.style.backgroundColor = '#ccc'
+  hide($timeHeader)
+  show($resultHeader)
 }
 
-// Создание квадрата
+function handleBoxClick(event) {
+  if (!isGameStarted) {
+    return
+  }
+
+  if (event.target.dataset.box) {
+    score++
+    renderBox()
+  }
+}
+
 function renderBox() {
   $game.innerHTML = ''
   var box = document.createElement('div')
@@ -50,19 +85,18 @@ function renderBox() {
   var maxTop = gameSize.height - boxSize
   var maxLeft = gameSize.width - boxSize
 
- 
   box.style.height = box.style.width = boxSize + 'px'
   box.style.position = 'absolute'
   box.style.backgroundColor = '#000'
-  box.style.top = getRandom(0, maxTop)+ 'px'
+  box.style.top = getRandom(0, maxTop) + 'px'
   box.style.left = getRandom(0, maxLeft) + 'px'
   box.style.cursor = 'pointer'
   box.setAttribute('data-box', 'true')
 
   $game.insertAdjacentElement('afterbegin', box)
+
 }
 
-//создание числа
-function getRandom(min,max){
-  return Math.floor(Math.random() * (max-min) + min)
+function getRandom(min, max) {
+  return Math.floor(Math.random() * (max - min) + min)
 }
