2537271439dfc9f3dac56d40a5f3b2df02c2f5d1
Marat Gumerov <gumerovm74@gmail.com>
2020-06-22 10:50:23 +0300

    localStorage

--- src/script.js
+++ src/script.js
@@ -1,11 +1,33 @@
-document.querySelector('#alert').addEventListener('click', function(){
-  alert('Вы успешно кликнули по кнопке');
-  
+// LocalStorage
+
+
+document.querySelector('button').addEventListener('click', function(event){
+  var value = document.querySelector('input').value
+
+  var obj = {
+    text:value
+  }
+
+  localStorage.setItem('headerText', JSON.stringify(obj))
 })
 
-document.querySelector('#confirm').addEventListener('click', function () {
-  var decision = confirm('Вы успешно кликнули по кнопке');
-  console.log(decision);
-  
+// document.addEventListener('DOMContentLoaded', function(){
 
+//   var text = localStorage.getItem('headerText')
+//   if (text && text.trim()){
+//   document.querySelector('h1').textContent = text
+//   }
+// })
+
+ document.addEventListener('DOMContentLoaded', function(){
+  var obg = {}
+  try {
+    var obj = JSON.parse(localStorage.getItem('headerText'))
+  } catch(e) {
+  }
+  
+  
+  if (obj && obj.text && obj.text.trim()){
+  document.querySelector('h1').textContent = obj.text
+  }
 })
